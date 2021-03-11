f98184cb35b32f9b50b6cbf47847951a9cbfe88a
github username <60378440+vmar13@users.noreply.github.com>
2021-03-11 17:06:36 -0500

    added controller actions for messages and conversations

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,25 @@
 class MessagesController < ApplicationController
   def index
+    messages = Message.all
+    render :index
   end
 
   def show
+    message = Message.find(params[:id])
   end
 
   def new
+    message = Message.new 
   end
 
   def create
+    message = Message.create!(message_params)
+    redirect_to message_path(message)
+  end
+
+  private 
+
+  def message_params
+    params.require(:message).permit(:text, :conversation_id)
   end
 end
--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,13 +1,25 @@
 class ThoughtsController < ApplicationController
   def index
+    thoughts = Thought.all
+    render :index
   end
 
   def show
+    thought = Thought.find(params[:id])
   end
 
   def new
+    thought = Thought.new
   end
 
   def create
+    thought = Thought.create!(thought_params)
+    redirect_to thought_path(thought)
+  end
+
+  private 
+
+  def thought_params
+    params.require(:thought).permit(:text, :message_id)
   end
 end
