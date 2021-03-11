8b7081db7edefe831c3f470e69e178bab9433c83
github username <60378440+vmar13@users.noreply.github.com>
2021-03-11 17:21:29 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Conversation.destroy_all
+Message.destroy_all
+Thought.destroy_all
+
+20.times do
+    Conversation.create(title: Faker::Book.title, start_date: Faker::Date.in_date_period)
+end
+
+20.times do 
+    Message.create(text: Faker::Quote.famous_last_words, conversation_id: Conversation.all.sample.id)
+end
+
+20.times do
+    Thought.create(text: Faker::Lorem.words, message_id: Message.all.sample.id)
+end
+    
+  
