0b2057f9795e12c0de5182a3b265647b8a68bfb1
github username <60378440+vmar13@users.noreply.github.com>
2021-03-11 17:01:12 -0500

    added controller actions for conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,22 +1,38 @@
 class ConversationsController < ApplicationController
   def index
+    conversations = Conversation.all
+    render :index
   end
 
   def show
+    conversation = Conversation.find(params[:id])
   end
 
   def new
+    conversation = Conversation.new 
   end
 
   def create
+    conversation = Conversation.create!(conversation_params)
+    redirect_to conversation_path(conversation)
   end
 
   def edit
+    conversation = Conversation.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    conversation = Conversation.find(params[:id])
+    conversation.destroy
+    redirect_to conversations_path(conversations)
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:title, :start_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'thoughts/index'
-  get 'thoughts/show'
-  get 'thoughts/new'
-  get 'thoughts/create'
   resources :conversations
   resources :messages
+  resources :thoughts 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
