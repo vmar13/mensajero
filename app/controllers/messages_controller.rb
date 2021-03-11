4cc0bde2ac8ad557d437ea690c49489c30e637b2
github username <60378440+vmar13@users.noreply.github.com>
2021-03-11 17:57:23 -0500

    message show pg rendering

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,18 @@
 class MessagesController < ApplicationController
   def index
-    messages = Message.all
-    render :index
+    @messages = Message.all
   end
 
   def show
-    message = Message.find(params[:id])
+    @message = Message.find(params[:id])
   end
 
   def new
-    message = Message.new 
+    @message = Message.new 
   end
 
   def create
-    message = Message.create!(message_params)
+    @message = Message.create!(message_params)
     redirect_to message_path(message)
   end
 
